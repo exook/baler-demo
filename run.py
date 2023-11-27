bbcf22fcdd479013ebbd8c6b114797500ee17568
Alexander Ekman <alexander.ekman95@gmail.com>
2023-11-27 14:14:42 +0100

    Presentation used for SMARTHEP 2023 v1

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ import numpy as np
 
 # Define config
 config = config_module.Config
-config.compression_ratio = 100
-config.epochs = 1
+config.compression_ratio = 1000
+config.epochs = 1000
 config.early_stopping = False
 config.early_stopping_patience = 100
 config.min_delta = 0
 config.lr_scheduler = True
-config.lr_scheduler_patience = 50
+config.lr_scheduler_patience = 100
 config.model_name = "CFD_dense_AE"
 config.model_type = "dense"
 config.custom_norm = True
@@ -22,7 +22,7 @@ config.l1 = True
 config.reg_param = 0.001
 config.RHO = 0.05
 config.lr = 0.001
-config.batch_size = 5
+config.batch_size = 75
 config.test_size = 0.2
 config.data_dimension = 2
 config.apply_normalization = False
@@ -34,36 +34,57 @@ config.verbose = True
 # Run training
 input_data_path = "input/exafel_1.npz"
 output_path = "output/"
-model, normalization_features, loss_data = trainer_module.run(input_data_path, config)
-torch.save(model.state_dict(), output_path + "compressed_output/model.pt")
+# model, normalization_features, loss_data = trainer_module.run(input_data_path, config)
+# torch.save(model.state_dict(), output_path + "compressed_output/model.pt")
 
-# Run compression
-compressed, names, normalization_features, original_shape = compressor_module.run(
-    input_data_path,
-    output_path + "compressed_output/model_preBaked.pt",
-    normalization_features,
-    config,
-)
+# # Run compression
+# compressed, names, normalization_features, original_shape = compressor_module.run(
+#     input_data_path,
+#     output_path + "compressed_output/model.pt",
+#     normalization_features,
+#     config,
+# )
 
-# Save compressed file to disk
-np.savez_compressed(
-    output_path + "compressed_output/compressed.npz",
-    data=compressed,
-    names=names,
-    normalization_features=normalization_features,
-    original_shape=original_shape,
-)
+# # Save compressed file to disk
+# np.savez_compressed(
+#     output_path + "compressed_output/compressed.npz",
+#     data=compressed,
+#     names=names,
+#     normalization_features=normalization_features,
+#     original_shape=original_shape,
+# )
+# np.save(output_path + "loss_data.npy", loss_data)
 
-# Run decompression
-decompressed, names, original_shape = decompressor_module.run(
-    output_path + "compressed_output/model_preBaked.pt",
-    output_path + "compressed_output/compressed.npz",
-    config,
-)
+# # Run decompression
+# decompressed, names, original_shape = decompressor_module.run(
+#     output_path + "compressed_output/model.pt",
+#     output_path + "compressed_output/compressed.npz",
+#     config,
+# )
 
-# Save decompressed file to disk
-np.savez(
-    output_path + "decompressed_output/decompressed.npz",
-    data=decompressed,
-    names=names,
-)
+# # Save decompressed file to disk
+# np.savez(
+#     output_path + "decompressed_output/decompressed.npz",
+#     data=decompressed,
+#     names=names,
+# )
+
+original = np.load("input/exafel_1.npz")["data"]
+decompressed = np.load("output/decompressed_output/decompressed.npz")["data"]
+
+import imageio
+
+images = []
+for i in range(0, len(original)):
+    print(i)
+    fig = helper.plot2D(original[i], decompressed[i])
+    fig.savefig(output_path + "/plot_output/" + str(i) + ".png", bbox_inches="tight")
+    images.append(imageio.imread(output_path + "/plot_output/" + str(i) + ".png"))
+fig = helper.plot2D(original[0], decompressed[0])
+fig.savefig(output_path + "/plot_output/test.png", bbox_inches="tight")
+imageio.mimsave(output_path + "/plot_output/movie.gif", images)
+
+# images = []
+# for i in range(0, 75):
+#     images.append(imageio.imread(output_path + "/plot_output/" + str(i) + ".png"))
+# imageio.mimsave(output_path + "/plot_output/movie.gif", images)
